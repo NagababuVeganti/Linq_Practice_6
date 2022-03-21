ab8ce6b07d2cbbd370d899fcbd8055b49c22986f
NagababuVeganti <nagababu@usf.edu>
2022-03-21 17:53:41 -0400

    Added new courses and College information

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,9 +23,8 @@ namespace WebApplication1
         public async Task<IActionResult> Queries()
         {
 
-            /*
-             
-            //Select All Query
+           /*
+            //Select All records in the Enrollments table.
 
            List<Enrollment> allrecords= dbContext.Enrollments.Include(s => s.Student).Include(s => s.Course).Include(s=>s.Course.College).ToList();
             Console.WriteLine("Feteched Records");
@@ -33,34 +32,37 @@ namespace WebApplication1
             ViewData["data"] = allrecords;
             return View("data");
             
-             */
-
+            */
             
 
-            //Select by Name Query
+           
+
+            //Select All records in the Enrollments table for student Ross.
 
             List<Enrollment> ross_records = dbContext.Enrollments.Include(s => s.Student).Include(s => s.Course).Include(s => s.Course.College).Where(s => s.Student.Name.Equals("Ross")).ToList();
             Console.WriteLine("Feteched Records for Ross");
             ViewData["title"] = "Records of Ross";
             ViewData["data"] = ross_records;
             return View("data");
+
             
-           
 
 
-           /*
 
-            //Task for Students to query based on Course Abbreviation
 
-            //Select Query based on Collge abbreviation
+            /*
 
-            List<Enrollment> MCOB_records = dbContext.Enrollments.Include(s => s.Student).Include(s => s.Course).Include(s => s.Course.College).Where(s => s.Course.College.Abbreviation.Contains("CoE")).ToList();
-            Console.WriteLine("Feteched Records for MCOB College");
-            ViewData["title"] = "Records of MCOB College:";
-            ViewData["data"] = MCOB_records;
-            return View("data");
+             //Task for Students to query based on Course Abbreviation
 
-            */
+             //Select All records in the Enrollments table with  college Abbreviation "CoE"
+
+             List<Enrollment> MCOB_records = dbContext.Enrollments.Include(s => s.Student).Include(s => s.Course).Include(s => s.Course.College).Where(s => s.Course.College.Abbreviation.Contains("CoE")).ToList();
+             Console.WriteLine("Feteched Records for MCOB College");
+             ViewData["title"] = "Records of MCOB College:";
+             ViewData["data"] = MCOB_records;
+             return View("data");
+
+             */
 
 
 
@@ -83,9 +85,9 @@ namespace WebApplication1
             return View("group");
 
             */
-            
 
-           
+
+
             /*
              
             //Excercise for Student 
@@ -106,7 +108,7 @@ namespace WebApplication1
             return View("collgeGroupby");
 
             */
-            
+
 
         }
 
@@ -115,14 +117,41 @@ namespace WebApplication1
         {
             Random rnd = new Random();
                 
-            string[] Colleges = {"Muma College of Business, MCOB",
-                "College of Engineering, CoE", "College of Arts and Sciences, CAS"};
-            string[] Courses = {"ISM 6225, Distributed Information Systems",
+            string[] Colleges = {
+                                 "Muma College of Business, MCOB",
+                                 "College of Engineering, CoE",
+                                 "College of Arts and Sciences, CAS",
+                                 "College of Nursing, CON",
+                                 "Morsani College of Medicine,MCOM",
+                                 "College of Public Health,COPH"
+            };
+            string[] Courses = {
+                "ISM 6225, Distributed Information Systems",
                 "ISM 6218, Advanced Database Management Systems",
-                "ISM 6328, Information Security and IT Risk Management"};
-            string[] Students = { "Monica", "Sara","Adam","Jude","Callie","Ross","Stark","Chandler","Phoebe","Carrie","Triston"};
+                "ISM 6137, Statistical Data Mining",
+                "ISM 6419, Data Visualization",
+                "ISM 6930, Blockchain Fundamentals",
+                "ISM 6562, Big Data for Business",
+                "ISM 6328, Information Security and IT Risk Management",
+                "QMB 6304, Analytical Methods For Business",
+                "ISM 6136, Data Mining",
+                "NUR 3125, Pathophysiology for Nursing Practice",
+                "NUR 3145, Pharmacology in Nursing Practice",
+                "NUR 4165, Nursing Inquiry",
+                "NUR 3078, Information Technology Skills for Nurses",
+                "NUR 4169, Evidence-Based Practice for Baccalaureate Prepared Nurse",
+                "NUR 4634, Population Health",
+                "NSP 3147, Web-Based Education for Staff Development"
+
+            };
+            
+            string[] Students = {
+                "Monica", "Sara","Adam","Jude","Callie","Ross","Stark",
+                "Chandler","Phoebe","Carrie","Tristan","sally","Robert",
+                "Sid","Warner","Joey","Andy","Conner","Ruby","Kate"
+            };
             string[] Grades = { "A", "A-", "B+", "B", "B-" };
-            int[] scores = { 95, 91, 87, 82, 75 };
+            int[] scores = { 95, 91, 87, 82, 75,66,55,80,63,90,45,60};
 
             College[] colleges = new College[Colleges.Length];
             Course[] courses = new Course[Courses.Length];
